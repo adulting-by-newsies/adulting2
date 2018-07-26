d01f9e2fadf528745994f70da3b59c9789c5c0b6
kilcannon <scott.bouchey@gmail.com>
2018-07-26 15:06:28 -0700

    updated with carousel feature partially working

--- client/components/Homepage/Slider.js
+++ client/components/Homepage/Slider.js
@@ -1,58 +1,109 @@
-import React, { Component } from 'react';
-import SlideOne from './SlideOne';
-import SlideTwo from './SlideTwo';
-import SlideThree from './SlideThree';
-import SlideFour from './SlideFour';
-import SlideFive from './SlideFive';
-import RightArrow from './RightArrow';
-import LeftArrow from './LeftArrow';
+import React from "react";
+// import ReactDOM from "react-dom";
+// import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import { React_Bootstrap_Carousel } from "react-bootstrap-carousel";
 
+const styles = { height: 400, width: "100%" };
 
-export default class Slider extends Component {
+class Demo extends React.PureComponent {
   constructor(props) {
     super(props);
-
     this.state = {
-      slideCount: 1
-    }
-
-  this.nextSlide = this.nextSlide.bind(this);
-  this.previousSlide = this.previousSlide.bind(this);
-
+      autoplay: true
+    };
   }
-
-  nextSlide() {
-    if (this.state.slideCount === 5) {
-      this.setState({ slideCount: this.state.slideCount = 1})
+  onSelect = (active, direction) => {
+    console.log(`active=${active} && direction=${direction}`);
+  };
+  slideNext = () => {
+    this.slider.slideNext();
+  };
+  slidePrev = () => {
+    this.slider.slidePrev();
+  };
+  _changeIcon = () => {
+    let { leftIcon, rightIcon } = this.state;
+    if (leftIcon && rightIcon) {
+      this.setState({
+        leftIcon: undefined,
+        rightIcon: undefined
+      });
+    } else {
+      this.setState({
+        leftIcon: <span className="glyphicon glyphicon-glass" />,
+        rightIcon: <span className="glyphicon glyphicon-music" />
+      });
     }
-    else {
-      this.setState({ slideCount: this.state.slideCount + 1 })
-    }
-  }
-
-  previousSlide() {
-    if (this.state.slideCount === 1) {
-      this.setState({ slideCount: this.state.slideCount = 5})
-    }
-    else {
-      this.setState({ slideCount: this.state.slideCount - 1 })
-    }
-  }
-
+  };
   render() {
+    let { leftIcon, rightIcon } = this.state;
     return (
-      <div className="slider">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12" style={{ paddingTop: "20px" }}>
+            <div className="btn-group">
+              <button type="button" className="btn btn-primary" onClick={this.slidePrev}>
+                Prev Article
+              </button>
+              <button type="button" className="btn btn-primary" onClick={this.slideNext}>
+                Next Article
+              </button>
+            </div>
+          </div>
+          <div className="col-md-12" style={{ marginTop: 20 }}>
+            <React_Bootstrap_Carousel
+              animation={true}
+              autoplay={this.state.autoplay}
+              slideshowSpeed={7000}
+              defaultActiveIndex={0}
+              leftIcon={leftIcon}
+              rightIcon={rightIcon}
+              onSelect={this.onSelect}
+              ref={r => (this.slider = r)}
+            >
+              <div style={{ height: 700 }}>
+                <img
+                  style={{ width: "100%", height: "100%" }}
+                  src="https://static01.nyt.com/images/2018/07/27/us/politics/27dc-obstruct1/07dc-tamper-superJumbo.jpg?quality=90&auto=webp"
+                />
+                <div className="carousel-caption">Mueller Examining Trump’s Tweets in Wide-Ranging Obstruction Inquiry</div>
+              </div>
+              <div style={{ height: 700 }}>
+                <img
+                  style={{ width: "100%", height: "100%" }}
+                  src="https://static01.nyt.com/images/2018/07/26/sports/26yankeesweb1/merlin_141691740_466ff265-2a8e-428b-934e-fe22c61a8ba4-superJumbo.jpg?quality=90&auto=webp"
+                />
+                <div className="carousel-caption">Yankees Leave Florida With No Homers and a Bigger A.L. East Deficit</div>
+              </div>
+              <div style={{ height: 700 }}>
+                <img
+                  style={{ width: "100%", height: "100%" }}
+                  src="https://images.tmz.com/2018/07/26/0726-john-schnatter-papa-johns-getty-composite-3.jpg"
+                />
+                <div className="carousel-caption">PAPA JOHNSUES PAPA JOHNS... They Did Me Dirty</div>
+              </div>
+              <div style={{ height: 700 }}>
+                <img
+                  style={{ width: "100%", height: "100%" }}
+                  src="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/720cs_80727X_Clouds_0.jpg?itok=TFZV4mf_"
+                />
+                <div className="carousel-caption">Science insurgents plot a climate model driven by artificial intelligence</div>
+              </div>
+              <div style={{ height: 700 }}>
+                <img
+                  style={{ width: "100%", height: "100%" }}
+                  src="https://techcrunch.com/wp-content/uploads/2018/04/gettyimages-944718240.jpeg?w=1390&crop=1"
+                />
+                <div className="carousel-caption">Facebook officially loses $123 billion in value</div>
+              </div>
+              
+            </React_Bootstrap_Carousel>
+          </div>
 
-        { this.state.slideCount === 1 ? <SlideOne /> : null }
-        { this.state.slideCount === 2 ? <SlideTwo /> : null }
-        { this.state.slideCount === 3 ? <SlideThree /> : null }
-        { this.state.slideCount === 4 ? <SlideFour /> : null }
-        { this.state.slideCount === 5 ? <SlideFive /> : null }
-        
-        <RightArrow nextSlide={this.nextSlide} />
-        <LeftArrow previousSlide={this.previousSlide} />
-
+        </div>
       </div>
     );
   }
 }
+
+export default Demo
--- client/components/pages/HomePage.js
+++ client/components/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Demo from '../Homepage/Slider.js'
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
           <h1 className="title is-1">
             Home Page
           </h1>
+            <Demo />
         </div>
       </div>
     </div>
