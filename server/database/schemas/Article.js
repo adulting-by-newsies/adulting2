7681d68682b97f50414d4b86fb8a16da83180495
wodjafink <wodjafink@gmail.com>
2018-07-25 22:52:31 -0700

    Adding nates article model

---
+++ server/database/schemas/Article.js
@@ -0,0 +1,32 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new NoteSchema object
+// This is similar to a Sequelize model
+var ArticleSchema = new Schema({
+  snapshotSum: {
+    type: String
+    required: true
+  },
+  href: {
+    type: String
+    required: true
+  },
+  category: {
+    type: String
+    required: true
+  },
+  ID: {
+    type: Number
+    required: true
+  },
+
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Note model
+module.exports = Article;
