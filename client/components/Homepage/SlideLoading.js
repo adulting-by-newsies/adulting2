a17c89b6a32605de34d29df71b7e28ad496be462
wodjafink <wodjafink@gmail.com>
2018-07-25 23:57:48 -0700

    Got the slider to render on homepage

---
+++ client/components/Homepage/SlideLoading.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const SlideLoading = (props) => {
+
+  let background = {
+      backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center'
+  }
+
+  return <div className="slide"></div>
+}
+
+export default SlideLoading;
--- client/components/pages/HomePage.js
+++ client/components/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Slider from '../Homepage/Slider.js'
 export default function HomePage() {
   return (
     <div className="home-page">
@@ -7,6 +7,7 @@ export default function HomePage() {
         <div className="container">
           <h1 className="title is-1">
             Home Page
+            <Slider/>
           </h1>
         </div>
       </div>
