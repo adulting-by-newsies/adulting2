2a352f6c627109ecc2925b2c47a13f88cc201789
wodjafink <wodjafink@gmail.com>
2018-07-28 11:43:18 -0700

    Completely reorganized the code to use promises, but needs to be optimized to kick off 10 API calls in a row and then return the results array

--- server/routes/newsAPI.js
+++ server/routes/newsAPI.js
@@ -1,70 +1,60 @@
+// This function will step through the various categories and append 10 articles from each to
+// a massive results array
+// var request = require("request");
 require('dotenv').config()
 var axios = require("axios");
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
 const interestCategoriesTop = ["sports", "politics","science", "technology", "health", "business"]
 const interestCategoriesEverything = ["culture", "worldnews","cooking", "lifestyle"]
+var userDetails;
 
-// This function will step through the various categories and append 10 articles from each to
-// a massive results array
-function scrapeArticles() {
-  var results = [];
-  var total = 0;
-
-  // To query /v2/top-headlines
-  // All options passed to topHeadlines are optional, but you need to include at least one of them
-  interestCategoriesTop.forEach(function(theCategory){
-    // console.log("newsAPI: category GET for " + theCategory)
+function topCategoryAPICall(theCategory) {
+  return new Promise((resolve, reject) => {
     newsapi.v2.topHeadlines({
       country: 'us',
       category: theCategory
     }).then(response => {
-      // console.log(JSON.stringify(response));
-      var i = 0;
-      // console.log("Length is " + response.length);
+      resolve(response);
+    })
+  })
+}
 
-      // console.log(response.articles);
-      response.articles.forEach(function(article){
-        console.log(article.title);
-        results.push(article);
-        i++;
-        total++;
-        if (i > 10)
-          return;
-      })
-      interestCategoriesEverything.forEach(function(theCategory){
-        // console.log("newsAPI: query GET for " + theCategory)
-        newsapi.v2.everything({
-          q: theCategory
-        }).then(response => {
-          // console.log(JSON.stringify(response));
-          var i = 0;
-          // console.log("Length is " + response.length);
+function everythingCategoryAPICall(theCategory) {
+  return new Promise((resolve, reject) => {
+    newsapi.v2.everything({
+      q: theCategory
+    }).then(response => {
+      resolve(response);
+    })
+  })
+}
 
-          // console.log(response.articles);
-          response.articles.forEach(function(article){
-            console.log(article.title);
-            results.push(article);
-            i++;
-            total++;
-            if (i > 10)
-              return;
-          })
-          console.log("newsAPI: Returning " + total + " articles across 10 categories");
-          console.log("Size is " + results.length)
-          return results;
-          
-        });
-      });
-      
-    });
-  });
+var errHandler = function(err) {
+    console.log("ERROR");
+    console.log(err);
+}
 
+function main() {
+  var listOfArticles = [];
+  var total = 0;
+  var dataPromise = everythingCategoryAPICall(interestCategoriesEverything[0]);
+  // var dataPromise = topCategoryAPICall(interestCategoriesTop[0]);
+  // Get user details after that get followers from URL
+
+  dataPromise.then(function(result){
+    var i = 0;
+    result.articles.some(function(article){
+      total++;
+      i++;
+      console.log("Title : " + article.title)
+      listOfArticles.push(article);
+      return i === 10;
+    })
+    console.log("Captured " + total + " articles")
+  })
 
 }
-function scraping() {
-  return new scrapeArticles
-}
-module.exports = { scraping,
-scrapeArticles
-}
+
+
+main();
