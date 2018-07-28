4699c91ed7f38067aafbf99ad8f20ff68f246dc6
wodjafink <wodjafink@gmail.com>
2018-07-28 13:18:42 -0700

    Okay documented the code so that it should be fairly obvious what is going on in this code

--- server/routes/newsAPI.js
+++ server/routes/newsAPI.js
@@ -50,6 +50,9 @@ function scrapeArticles() {
   var listOfArticles = [];
   var total = 0;
 
+  // Kick off all 10 promises at once, whee!!!!
+  // This will return a massive object that is an aggregate
+  // of all the results
   return Promise.all(
     [
       topCategoryAPICall(interestCategoriesTop[0]), 
@@ -63,22 +66,29 @@ function scrapeArticles() {
       everythingCategoryAPICall(interestCategoriesEverything[2]),
       everythingCategoryAPICall(interestCategoriesEverything[3]),
     ]).then(function(values) {
+      // Each of the API calls returns 20 results per query, but we will
+      // chop off the first 10 of them.  Unfortunately the API doesn't offer
+      // a parameter to limit the number of results (or if it does it isn't
+      // documented on the website)
       for (var i = 0; i < 10; i++){
-        // console.log("Do iteration " + i)
         var j = 0;
+        // Use .some here, it's a truncated version of forEach that allows
+        // you to escape early
         values[i].articles.some(function(article){
           total++;
           j++;
+          // Here we add a category field to each article object, this should
+          // make it easier to parse through so you know what class of article
+          // you're looking at
           article.category = allCategories[i]
-          // console.log("Title : " + article.title)
+          // Add to the array
           listOfArticles.push(article);
-          // console.log(article);
+          // This is the escape for when you've added 10 articles
           return j === 10;
         })
         console.log("Captured " + total + " articles")
       }
       console.log("Returning now ");
-      // console.log("Should get back " + listOfArticles)
       return listOfArticles;
   });
 
