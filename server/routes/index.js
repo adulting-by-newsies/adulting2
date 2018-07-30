6fd3fb1c14acbcf1d3a33da968c08573a7658b4c
wodjafink <wodjafink@gmail.com>
2018-07-29 17:56:15 -0700

    Exposing API routes through the router index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+const articles     = require('./articles')
 const auth         = require('./auth');
 const user         = require('./user');
 const users        = require('./users');
@@ -10,6 +11,7 @@ const router = express.Router();
 router.use('/api/auth', auth);
 router.use('/api/user', user);
 router.use('/api/users', users);
+router.use('/api/articles', articles);
 
 router.get('/api/tags', (req, res) => {
   res.send([
