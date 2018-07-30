37abf857b289da9785cb4022c93f0c54974a9737
wodjafink <wodjafink@gmail.com>
2018-07-29 17:57:39 -0700

    The articles file is part of the router and gives access to the methods

---
+++ server/routes/articles.js
@@ -0,0 +1,18 @@
+const { Article } = require('../database/schemas');
+const articleController = require("../database/controllers/articleController");
+const router = require("express").Router();
+module.exports = router;
+
+// Post route to write into the database for the articles
+// or return all articles
+router.route("/")
+  .get(articleController.findAll)
+  .post(articleController.create);
+
+// For a specific article ID, get or remove that article
+router
+  .route("/:id")
+  .get(articleController.findById)
+  .delete(articleController.remove);
+
+module.exports = router;
