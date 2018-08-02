82cf34c082696358fa2f19770eda62eb7fea5d76
wodjafink <wodjafink@gmail.com>
2018-08-01 19:02:50 -0700

    modified home page to be a component so we can concentrate API calls in one place and started reading from user database, next step is to build out article api reads

--- client/components/pages/HomePage.js
+++ client/components/pages/HomePage.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardOne from '../Homepage/CardOne.js';
 import CardTwo from '../Homepage/CardTwo.js';
 import CardThree from '../Homepage/CardThree.js';
 import CardFour from '../Homepage/CardFour.js';
 import CardFive from '../Homepage/CardFive.js';
 
-export default function HomePage() {
-  return (
-    <div className="home-page">
-      <div className="section">
-        <div className="container">
-          <h1 className="title is-1">
-            Welcome to your personalized article feed!
-          </h1>
-          <CardOne />
-          <CardTwo />
-          <CardThree />
-          <CardFour />
-          <CardFive />
+import {
+  postRegister, postLogin, postLogout, getUser, putUser, putUserPassword,
+} from '../../utils/api';
+
+export default class HomePage extends Component {
+
+  state = {
+    userArticleList: [],
+    userPreferences: [],
+    username: ""
+  }
+
+  componentDidMount() {
+    getUser().then(data => {
+      console.log(data.user);
+      this.setState({username: data.user.username});
+      console.log(data.user.preferences.length)
+      if (data.user.preferences.length === 0){
+        console.log("User has no preferences!")
+        //TODO: Write some code to build out a default homepage
+      } else {
+        //TODO: Handle all the cases to build out the article list
+        // with the preferences given here
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="home-page">
+        <div className="section">
+          <div className="container">
+            <h1 className="title is-1">
+              Welcome to your personalized article feed, {this.state.username}!
+            </h1>
+            <CardOne />
+            <CardTwo />
+            <CardThree />
+            <CardFour />
+            <CardFive />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
