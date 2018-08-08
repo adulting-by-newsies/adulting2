758ec37839e4961c53e6908a55935ce363d67876
wodjafink <wodjafink@gmail.com>
2018-08-07 20:16:42 -0700

    Pointing to mlab

--- server/config/environment.js
+++ server/config/environment.js
@@ -1,3 +1,3 @@
-process.env.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/adulting';
+process.env.DATABASE_URL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/adulting';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.PORT = process.env.PORT || 3000;
